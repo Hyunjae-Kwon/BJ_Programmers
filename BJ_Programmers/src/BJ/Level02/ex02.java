a7bc2e5ba47fb4626bcf2be551afe0b32182978f
Hyunjae Kwon <kwonfelix93@gmail.com>
2022-08-25 16:18:20 +0900

    Level 02

--- BJ_Programmers/src/BJ/Level02/ex02.java
+++ BJ_Programmers/src/BJ/Level02/ex02.java
@@ -1,5 +1,27 @@
 package BJ.Level02;
 
-public class ex02 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class ex02 {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int score = Integer.parseInt(st.nextToken());
+		
+		if( 90 <= score && score <= 100) {
+			System.out.println("A");
+		} else if (80 <= score && score <= 89) {
+			System.out.println("B");
+		} else if (70 <= score && score <= 79) {
+			System.out.println("C");
+		} else if (60 <= score && score <= 69) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
 }
